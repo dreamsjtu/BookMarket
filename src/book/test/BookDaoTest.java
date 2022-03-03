e6d3024a223169bfb590247d6cd66c42696f8898
Meng Li <dreamsjtu@gmail.com>
2022-03-03 22:08:52 +1300

    Create bookdao test template

---
+++ src/book/test/BookDaoTest.java
@@ -0,0 +1,34 @@
+package book.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BookDaoTest {
+
+	@Test
+	void testSaveBook() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testQueryBookById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteBookById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateBook() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testQueryBooks() {
+		fail("Not yet implemented");
+	}
+
+}
