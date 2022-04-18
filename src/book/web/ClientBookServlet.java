b0d04d78fe85185732960b6a16dcc383780f2f74
Meng Li <dreamsjtu@gmail.com>
2022-04-18 22:57:40 +1200

    Override doget method, otherwise will have 405 error

--- src/book/web/ClientBookServlet.java
+++ src/book/web/ClientBookServlet.java
@@ -22,6 +22,7 @@ public class ClientBookServlet extends BaseServlet {
    */
   protected void page(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     //Get the parameters(pageNumber,pageSize) from request
+    System.out.println("here");
     int pageNumber = WebUtils.parseInt(request.getParameter("pageNumber"),1);
     int pageSize = WebUtils.parseInt(request.getParameter("pageSize"),Page.PAGE_SIZE);
     //Call page method in bookServlet to get a page object
@@ -30,5 +31,9 @@ public class ClientBookServlet extends BaseServlet {
     request.setAttribute("page", page);
     //forward request to book_manager.jsp
     request.getRequestDispatcher("/pages/client/index.jsp").forward(request, response);
+  }
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    doPost(request,response);
   }
 }
