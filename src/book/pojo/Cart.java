20d9399a3037f012fe90f58afe879bd4c493c3b5
Meng Li <dreamsjtu@gmail.com>
2022-04-20 20:18:56 +1200

    Implemnet the methods in cart

--- src/book/pojo/Cart.java
+++ src/book/pojo/Cart.java
@@ -1,28 +1,57 @@
 package book.pojo;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Cart {
 
   private int totalCount;
   private BigDecimal totalPrice;
-  private List<CartItem> items;
+  private Map<Integer,CartItem> items  = new HashMap<>();
   
+  /**
+   * Add an item to cart.
+   * @param item 
+   */
   public void addItem(CartItem item) {
-    
+    CartItem c =  items.get(item.getId());
+   if(c!=null) {
+     c.setCount(c.getCount()+1);
+     c.setTotalPrice(c.getPrice().multiply(new BigDecimal(c.getCount())));
+     items.put(item.getId(),c );
+   }else {
+     items.put(item.getId(), item);
+   }
   }
   
+  /**
+   * Delete an item from cart.
+   * @param id id of the item for adding to cart
+   */
   public void deleteItem(int id) {
-    
+    items.remove(id);
   }
   
+  /**
+   * Update the quantity of an item in cart
+   * @param id
+   * @param count
+   */
   public void updateCount(int id, int count) {
-    
+    CartItem c = items.get(id);
+    if(c!=null) {
+      c.setCount(count);
+      c.setTotalPrice(c.getPrice().multiply(new BigDecimal(c.getCount())));
+    }  
   }
   
+  /**
+   * Clear the cart
+   */
   public void clearCart() {
-    
+    items.clear();
   }
   public int getTotalCount() {
     return totalCount;
@@ -30,7 +59,7 @@ public class Cart {
   public BigDecimal getTotalPrice() {
     return totalPrice;
   }
-  public List<CartItem> getItems() {
+  public Map<Integer, CartItem> getItems() {
     return items;
   }
   public void setTotalCount(int totalCount) {
@@ -39,7 +68,7 @@ public class Cart {
   public void setTotalPrice(BigDecimal totalPrice) {
     this.totalPrice = totalPrice;
   }
-  public void setItems(List<CartItem> items) {
+  public void setItems(Map<Integer, CartItem> items) {
     this.items = items;
   }
   @Override
@@ -47,7 +76,7 @@ public class Cart {
     return "Cart [totalCount=" + totalCount + ", totalPrice=" + totalPrice + ", items=" + items
         + "]";
   }
-  public Cart(int totalCount, BigDecimal totalPrice, List<CartItem> items) {
+  public Cart(int totalCount, BigDecimal totalPrice, Map<Integer, CartItem> items) {
     super();
     this.totalCount = totalCount;
     this.totalPrice = totalPrice;
