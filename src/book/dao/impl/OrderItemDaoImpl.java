a795ad5f971189cf5f3f456728564e96c738bf6d
Meng Li <dreamsjtu@gmail.com>
2022-04-22 15:03:36 +1200

    Create the test for OrderServiceImpl

--- src/book/dao/impl/OrderItemDaoImpl.java
+++ src/book/dao/impl/OrderItemDaoImpl.java
@@ -7,8 +7,8 @@ public class OrderItemDaoImpl extends BaseDao implements OrderItemDao{
 
   @Override
   public int saveOrderItem(OrderItem orderItem) {
-    String sql = "insert into t_order_item(`id`,`itemid`,`name`,`price`,`totalPrice`,`count`,`orderid`) values (?,?,?,?,?,?,?)";
-    return update(sql,orderItem.getId(),orderItem.getItemid(),orderItem.getName(),orderItem.getPrice(),orderItem.getTotalPrice(),orderItem.getCount(),orderItem.getOrderid());
+    String sql = "insert into t_order_item(`itemid`,`name`,`price`,`totalPrice`,`count`,`orderid`) values (?,?,?,?,?,?)";
+    return update(sql,orderItem.getItemid(),orderItem.getName(),orderItem.getPrice(),orderItem.getTotalPrice(),orderItem.getCount(),orderItem.getOrderid());
   }
 
 }
