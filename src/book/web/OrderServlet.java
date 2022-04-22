fde0d5d0e2c55a7499aba2d337f1b0b8c0292895
Meng Li <dreamsjtu@gmail.com>
2022-04-22 21:09:25 +1200

    Implement the showAllOrders method in OrderServlet and guide the link to OrderServlet once admin click on order mananging

--- src/book/web/OrderServlet.java
+++ src/book/web/OrderServlet.java
@@ -1,19 +1,27 @@
 package book.web;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import book.pojo.Cart;
+import book.pojo.Order;
 import book.pojo.User;
 import book.service.OrderService;
 import book.service.impl.OrderServiceImpl;
 
 public class OrderServlet extends BaseServlet {
+  private OrderService orderService = new OrderServiceImpl();
+  
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    doPost(request,response);
+  }
+  
   protected void createOrder(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-    OrderService orderService = new OrderServiceImpl();
     Cart cart = (Cart) request.getSession().getAttribute("cart");
     User user = (User) request.getSession().getAttribute("user");
     if(user==null) {
@@ -27,9 +35,8 @@ public class OrderServlet extends BaseServlet {
     response.sendRedirect(request.getContextPath()+"/pages/cart/checkout.jsp");
   }
   
-  @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-    doPost(request,response);
+  protected void showAllOrders(HttpServletRequest request, HttpServletResponse response) {
+    List<Order> allOrders = orderService.showAllOrders();
+    request.setAttribute("allOrders", allOrders);
   }
-
 }
