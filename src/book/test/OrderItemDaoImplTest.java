90a5abfc71394ee09a12005862d0f34f26ec4776
Meng Li <dreamsjtu@gmail.com>
2022-04-22 14:21:48 +1200

    Create the test for OrderItemDaoImpl

---
+++ src/book/test/OrderItemDaoImplTest.java
@@ -0,0 +1,19 @@
+package book.test;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+
+import book.dao.OrderItemDao;
+import book.pojo.OrderItem;
+
+class OrderItemDaoImplTest {
+
+  @Test
+  void testSaveOrderItem() {
+    OrderItemDao oid = new OrderItemDaoImpl();
+    OrderItem oi = new OrderItem(7968765,1,"hahaha",new BigDecimal(10.0),new BigDecimal(30.0),3,"23872938792");
+    oid.saveOrderItem(oi);
+  }
+
+}
